1583b2d6cf0ff0dfd1d22a162d22fc036f75de62
cjogles <jackson.ogles28@gmail.com>
2019-10-22 10:04:26 -0600

    added AddEvent

--- src/components/AddEvent.js
+++ src/components/AddEvent.js
@@ -1,66 +1,50 @@
 import React from 'react'
-import { Formik } from 'formik'
+import { Formik, Field } from 'formik'
 import { TextField } from 'formik-material-ui'
 import * as Yup from 'yup';
 
-function AddEvent() {
-    return (
-        <div>
-            <h1>Add Event</h1>
-            <Formik
-            initialValues={{ email: '', password: '' }}
-            validate={values => {
-                let errors = {};
-                if (!values.email) {
-                errors.email = 'Required';
-                } else if (
-                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                ) {
-                errors.email = 'Invalid email address';
-                }
-                return errors;
-            }}
-            onSubmit={(values, { setSubmitting }) => {
-                setTimeout(() => {
-                alert(JSON.stringify(values, null, 2));
-                setSubmitting(false);
-                }, 400);
-            }}
-            >
-            {({
-                values,
-                errors,
-                touched,
-                handleChange,
-                handleBlur,
-                handleSubmit,
-                isSubmitting,
-                /* and other goodies */
-            }) => (
-                <form onSubmit={handleSubmit}>
-                <input
-                    type="email"
-                    name="email"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.email}
-                />
-                {errors.email && touched.email && errors.email}
-                <input
-                    type="password"
-                    name="password"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.password}
-                />
-                {errors.password && touched.password && errors.password}
-                <button type="submit" disabled={isSubmitting}>
-                    Submit
-                </button>
-                </form>
-            )}
-            </Formik>
-        </div>
-    );
-}
+function AddEvent () {
+    <div>
+      <h1>My Form</h1>
+      <Formik
+        initialValues={{ email: '', color: 'red', firstName: '' }}
+        onSubmit={(values, actions) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            actions.setSubmitting(false);
+          }, 1000);
+        }}
+        render={(props: FormikProps<Values>) => (
+          <form onSubmit={props.handleSubmit}>
+            <Field type="email" name="email" placeholder="Email" />
+            <Field component="select" name="color">
+              <option value="red">Red</option>
+              <option value="green">Green</option>
+              <option value="blue">Blue</option>
+            </Field>
+            <Field name="firstName" component={CustomInputComponent} />
+            <Field
+              name="lastName"
+              render={({ field /* _form */ }) => (
+                <input {...field} placeholder="lastName" />
+              )}
+            />
+            <button type="submit">Submit</button>
+          </form>
+        )}
+      />
+    </div>
+};-
+  
+  const CustomInputComponent = ({
+    field, // { name, value, onChange, onBlur }
+    form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+    ...props
+  }) => (
+    <div>
+      <input type="text" {...field} {...props} />
+      {touched[field.name] &&
+        errors[field.name] && <div className="error">{errors[field.name]}</div>}
+    </div>
+  );
 export default AddEvent;
