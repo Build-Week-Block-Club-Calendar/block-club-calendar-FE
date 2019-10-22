2790d58830388bbbe78969a1d94bdbb6f8debb4c
gelinas <jlgelinas@gmail.com>
2019-10-22 14:38:18 -0400

    styled event

--- src/components/CalendarEvent.js
+++ src/components/CalendarEvent.js
@@ -4,11 +4,9 @@ import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
 
 const useStyles = makeStyles(theme => ({
   card: {
@@ -22,17 +20,19 @@ const useStyles = makeStyles(theme => ({
   topBlock : {
     display: 'flex'
   },
-  details: {
+  content: {
     display: 'flex',
     flexDirection: 'column',
     width: '50%'
   },
-  content: {
-    flex: '1 0 auto',
+  details: {
+    border: "2px solid #338a3e",
+    padding: 10
   },
   controls: {
     display: 'flex',
     alignItems: 'center',
+    justifyContent: 'space-around',
     paddingLeft: theme.spacing(1),
     paddingBottom: theme.spacing(1),
   },
@@ -67,30 +67,28 @@ export default function CalendarEvent(props) {
     return (
         <Card className={classes.card}>
           <div className={classes.topBlock}>
-            <CardContent className={classes.details}>
-              <Typography variant="h4" component="h3">
+            <CardContent className={classes.content}>
+              <Typography variant="h4" component="h3" align ="center" gutterBottom="true">
                 {event.Title}
               </Typography>
-              <Typography variant="h4" component="h3">
-                {event.Date}
-              </Typography>
-              <Typography variant="body2" component="p">
-                Location: {event.Location}
-              </Typography>
-              <Typography variant="body2" component="p">
-                Time: {event.Time}
-              </Typography>
-              <CardActions disableSpacing>
-                  <IconButton aria-label="going to event">
-                    <FavoriteIcon />
-                  </IconButton>
-                  <IconButton aria-label="share">
-                    <ShareIcon />
-                  </IconButton>
-              </CardActions>
+              <div className={classes.details}>
                 <Typography variant="body2" component="p">
-                {event.Description}
+                  Location: {event.Location}
                 </Typography>
+                <Typography variant="body2" component="p">
+                  Date: {event.Date}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  Time: {event.Time}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  Description: {event.Description}
+                </Typography>
+              </div>
+              <CardActions className={classes.controls}>
+                <Button variant="contained" color="primary">Share</Button>
+                <Button variant="contained" color="primary">Going</Button>
+              </CardActions>
             </CardContent>
             <CardMedia
                 className={classes.media}
