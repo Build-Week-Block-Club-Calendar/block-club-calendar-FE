f0939d0528886a2bd0187d4322f1c25b9b917cfb
gelinas <jlgelinas@gmail.com>
2019-10-22 13:59:35 -0400

    updated event card

--- src/components/CalendarEvent.js
+++ src/components/CalendarEvent.js
@@ -12,9 +12,6 @@ import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-// import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-// import Collapse from '@material-ui/core/Collapse';
-// import clsx from 'clsx';
 
 const useStyles = makeStyles(theme => ({
   card: {
@@ -22,10 +19,34 @@ const useStyles = makeStyles(theme => ({
     maxWidth: 1000,
     marginTop: 15,
     marginBottom: 15,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  topBlock : {
+    display: 'flex'
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '50%'
+  },
+  content: {
+    flex: '1 0 auto',
+  },
+  controls: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingLeft: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
   },
   media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
+    border: "1px solid black"
+  },
+  bottomBlock: {
+    marginTop: 15,
+    marginBottom: 15,
+    display: 'flex',
+    justifyContent: 'center'
   },
   expand: {
     transform: 'rotate(0deg)',
@@ -48,75 +69,43 @@ export default function CalendarEvent(props) {
 
     return (
         <Card className={classes.card}>
-            <CardHeader
-                avatar={
-                <Avatar aria-label="recipe" className={classes.avatar}>
-                    R
-                </Avatar>
-                }
-                action={
-                <IconButton aria-label="settings">
-                    <MoreVertIcon />
-                </IconButton>
-                }
-                title={event.Title}
-                subheader={event.Date}
-            />
+          <div className={classes.topBlock}>
+            <div className={classes.details}>
+              <CardHeader
+                  avatar={
+                  <Avatar aria-label="recipe" className={classes.avatar}>
+                      R
+                  </Avatar>
+                  }
+                  title={event.Title}
+                  subheader={event.Date}
+              />
+              <CardContent>
+                  <Typography variant="body2" component="p">
+                  Location: {event.Location}
+                  </Typography>
+                  <Typography variant="body2" component="p">
+                  Time: {event.Time}
+                  </Typography>
+              </CardContent>
+              <CardActions disableSpacing>
+                  <IconButton aria-label="going to event">
+                    <FavoriteIcon />
+                  </IconButton>
+                  <IconButton aria-label="share">
+                    <ShareIcon />
+                  </IconButton>
+              </CardActions>
+            </div>
             <CardMedia
                 className={classes.media}
                 image={event.Image}
                 title={event.Title}
             />
-            <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p">
-                {event.Description}
-                </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-                <IconButton aria-label="going to event">
-                <FavoriteIcon />
-                </IconButton>
-                <IconButton aria-label="share">
-                <ShareIcon />
-                </IconButton>
-                {/* <IconButton
-                className={clsx(classes.expand, {
-                    [classes.expandOpen]: expanded,
-                })}
-                onClick={handleExpandClick}
-                aria-expanded={expanded}
-                aria-label="show more"
-                >
-                <ExpandMoreIcon />
-                </IconButton> */}
-            </CardActions>
-            {/* <Collapse in={expanded} timeout="auto" unmountOnExit>
-                <CardContent>
-                <Typography paragraph>Method:</Typography>
-                <Typography paragraph>
-                    Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                    minutes.
-                </Typography>
-                <Typography paragraph>
-                    Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                    heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-                    browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-                    and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                    pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-                    saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-                </Typography>
-                <Typography paragraph>
-                    Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-                    without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-                    medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-                    again without stirring, until mussels have opened and rice is just tender, 5 to 7
-                    minutes more. (Discard any mussels that don’t open.)
-                </Typography>
-                <Typography>
-                    Set aside off of the heat to let rest for 10 minutes, and then serve.
-                </Typography>
-                </CardContent>
-            </Collapse> */}
+          </div>
+          <div className={classes.bottomBlock}>
+            <p>{event.Description}</p>
+          </div>
         </Card>   
     );
 }
